da607b4840c8bee5c853df5c68921a6a3bdb2ce4
Joel Azemar <joel.azemar@gmail.com>
2022-12-21 16:14:21 +0100

    Initial Commit

--- spec/support/shared_examples/validators/allows_prefix_zero.rb
+++
@@ -1,9 +0,0 @@
-shared_examples "allows_prefix_zero" do
-  context "with prefix zero" do
-    before { instance.string1 = "0000" + instance.string1 }
-
-    it "is valid" do
-      expect(subject).to eql true
-    end
-  end
-end
--- spec/support/shared_examples/validators/allows_suffix_zero.rb
+++
@@ -1,9 +0,0 @@
-shared_examples "allows_suffix_zero" do
-  context "with suffix zero" do
-    before { instance.string1 += "0000" }
-
-    it "is valid" do
-      expect(subject).to eql true
-    end
-  end
-end
